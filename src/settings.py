b614bce452f0a1c54a1eb167567d8fce4cbb9fab
Wang Tai <i@wangtai.me>
2022-10-14 14:00:10 +0800

    fix nullpointer

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ import os
 
 WECHAT_APP_ID = os.getenv('WECHAT_APP_ID')
 WECHAT_APP_SECRET = os.getenv('WECHAT_APP_SECRET')
-NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_DATABASE_ID = 'e5c7b98ea3d84f8f889e4c873d5ea2de'
 
 # assets location base ./src/
